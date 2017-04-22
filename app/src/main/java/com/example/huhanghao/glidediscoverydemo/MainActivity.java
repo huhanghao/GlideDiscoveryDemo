00bc0536d3c6242441ed75148ee98a06c4fc5093
huhanghao <517072971@qq.com>
2017-04-22 09:12:30 +0800

    测试了下

--- app/src/main/java/com/example/huhanghao/glidediscoverydemo/MainActivity.java
+++ app/src/main/java/com/example/huhanghao/glidediscoverydemo/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.huhanghao.glidediscoverydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image_view);
+    }
+
+    public void loadImage(View view) {
+        String url = "http://cn.bing.com/az/hprichbg/rb/Dongdaemun_ZH-CN10736487148_1920x1080.jpg";
+        String gifUrl = "http://p1.pstatp.com/large/166200019850062839d3";
+        Glide.with(this).load(url).placeholder(R.mipmap.ic_launcher).diskCacheStrategy(DiskCacheStrategy.NONE).error(R.mipmap.bg_file).into(imageView);
     }
 }
